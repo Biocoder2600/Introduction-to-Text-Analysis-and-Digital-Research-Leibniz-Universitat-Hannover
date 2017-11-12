49be1806cbad383769d25bc638facc02ef0753e6
justinchuntingho <zjustin334@gmail.com>
2017-11-12 14:51:17 +0000

    webscrapping

---
+++ webscrapping.R
@@ -0,0 +1,191 @@
+#install.packages('rvest')
+
+#Loading the rvest package
+library('rvest')
+library('magrittr')
+
+#Specifying the url for desired website to be scrapped
+url <- 'http://www.imdb.com/search/title?count=100&release_date=2017,2017&title_type=feature'
+
+#Reading the HTML code from the website
+webpage <- read_html(url)
+
+#Using CSS selectors to scrap the rankings section
+rank_data_html <- html_nodes(webpage,'.text-primary')
+
+#Converting the ranking data to text
+rank_data <- html_text(rank_data_html) %>% as.numeric()
+head(rank_data)
+
+#Using CSS selectors to scrap the title section
+title_data_html <- html_nodes(webpage,'.lister-item-header a')
+
+#Converting the title data to text
+title_data <- html_text(title_data_html)
+head(title_data)
+
+#Using CSS selectors to scrap the description section
+description_data_html <- html_nodes(webpage,'.ratings-bar+ .text-muted')
+
+#Converting the description data to text
+description_data <- html_text(description_data_html)
+
+#Let's have a look at the description data
+head(description_data)
+
+#Data-Preprocessing: removing '\n'
+description_data<-gsub("\n","",description_data)
+
+#Let's have another look at the description data 
+head(description_data)
+
+#Using CSS selectors to scrap the Movie runtime section
+runtime_data_html <- html_nodes(webpage,'.text-muted .runtime')
+
+#Converting the runtime data to text
+runtime_data <- html_text(runtime_data_html)
+
+#Let's have a look at the runtime
+head(runtime_data)
+
+
+#Data-Preprocessing: removing mins and converting it to numerical
+
+runtime_data<-gsub(" min","",runtime_data)
+runtime_data<-as.numeric(runtime_data)
+
+#Let's have another look at the runtime data
+head(runtime_data)
+
+#Using CSS selectors to scrap the Movie genre section
+genre_data_html <- html_nodes(webpage,'.genre')
+
+#Converting the genre data to text
+genre_data <- html_text(genre_data_html)
+
+#Let's have a look at the runtime
+head(genre_data)
+
+#Data-Preprocessing: removing \n
+genre_data<-gsub("\n","",genre_data)
+
+#Data-Preprocessing: removing excess spaces
+genre_data<-gsub(" ","",genre_data)
+
+#taking only the first genre of each movie
+genre_data<-gsub(",.*","",genre_data)
+
+#Let's have another look at the genre data
+head(genre_data)
+
+#Using CSS selectors to scrap the IMDB rating section
+rating_data_html <- html_nodes(webpage,'.ratings-imdb-rating strong')
+
+#Converting the ratings data to text
+rating_data <- html_text(rating_data_html)
+
+#Let's have a look at the ratings
+head(rating_data)
+
+#Data-Preprocessing: converting ratings to numerical
+rating_data<-as.numeric(rating_data)
+
+#Let's have another look at the ratings data
+head(rating_data)
+
+#Using CSS selectors to scrap the votes section
+votes_data_html <- html_nodes(webpage,'.sort-num_votes-visible span:nth-child(2)')
+
+#Converting the votes data to text
+votes_data <- html_text(votes_data_html)
+
+#Let's have a look at the votes data
+head(votes_data)
+
+#Data-Preprocessing: removing commas
+votes_data<-gsub(",","",votes_data)
+
+#Data-Preprocessing: converting votes to numerical
+votes_data<-as.numeric(votes_data)
+
+#Let's have another look at the votes data
+head(votes_data)
+
+#Using CSS selectors to scrap the directors section
+directors_data_html <- html_nodes(webpage,'.text-muted+ p a:nth-child(1)')
+
+#Converting the directors data to text
+directors_data <- html_text(directors_data_html)
+
+#Let's have a look at the directors data
+head(directors_data)
+
+
+#Data-Preprocessing: converting directors data into factors
+
+#Using CSS selectors to scrap the actors section
+actors_data_html <- html_nodes(webpage,'.lister-item-content .ghost+ a')
+
+#Converting the gross actors data to text
+actors_data <- html_text(actors_data_html)
+
+#Let's have a look at the actors data
+head(actors_data)
+
+
+#Using CSS selectors to scrap the metascore section
+metascore_data_html <- html_nodes(webpage,'.metascore')
+
+#Converting the runtime data to text
+metascore_data <- html_text(metascore_data_html)
+
+#Let's have a look at the metascore 
+head(metascore_data)
+
+
+#Data-Preprocessing: removing extra space in metascore
+metascore_data<-gsub(" ","",metascore_data)
+
+#Lets check the length of metascore data
+length(metascore_data)
+
+
+
+# Better approach
+split_html <- html_nodes(webpage,'.lister-item-content') 
+(split_html[1])
+
+# Testing
+metascore_data_test <- c()
+for (i in 1:length(split_html)){
+  if (length(html_nodes(split_html[i],'.metascore')) == 0){
+    metascore_data_item <- NA
+  } else {
+    metascore_data_item <- html_nodes(split_html[i],'.metascore') %>% html_text()
+  }
+  metascore_data_test <- c(metascore_data_test, metascore_data_item)
+}
+metascore_data_test<-gsub(" ","",metascore_data_test)
+length(metascore_data_test)
+
+# Even better approach
+node_or_na <- function(x,node){
+  final_list <- c()
+  for (i in 1:length(x)){
+    if (length(html_nodes(x[i],node)) == 0){
+      list_item <- NA
+    } else {
+      list_item <- html_nodes(x[i],node) %>% html_text()
+    }
+    final_list <- c(final_list, list_item)
+  }
+  return(final_list)
+}
+
+metascore_data <- node_or_na(test,'.metascore')
+metascore_data_test<-gsub(" ","",metascore_data_test)
+metascore_data_test
+
+gross_data <- node_or_na(test, ".ghost~ .text-muted+ span")
+
+df <- cbind(rank_data, title_data, description_data, genre_data, rating_data, runtime_data, votes_data, directors_data, metascore_data, gross_data) %>% as.data.frame()
